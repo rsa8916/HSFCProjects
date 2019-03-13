ade7818705dcaa43b9ec44ee0a9e0cb3f1094d90
rsa8916 <46599719+rsa8916@users.noreply.github.com>
2019-03-13 10:00:18 +0000

    School records
    school records

--- code/SchoolRecords/SchoolRecords/Form1.Designer.cs
+++ code/SchoolRecords/SchoolRecords/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.BUTTON = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -52,11 +53,23 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Under Development";
             // 
+            // BUTTON
+            // 
+            this.BUTTON.Location = new System.Drawing.Point(82, 158);
+            this.BUTTON.Name = "BUTTON";
+            this.BUTTON.Size = new System.Drawing.Size(99, 44);
+            this.BUTTON.TabIndex = 2;
+            this.BUTTON.Text = "PUSH";
+            this.BUTTON.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BUTTON.UseVisualStyleBackColor = true;
+            this.BUTTON.Click += new System.EventHandler(this.BUTTON_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.BUTTON);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -71,6 +84,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BUTTON;
     }
 }
 
--- code/SchoolRecords/SchoolRecords/Form1.cs
+++ code/SchoolRecords/SchoolRecords/Form1.cs
@@ -12,6 +12,7 @@ namespace SchoolRecords
 {
     public partial class Form1 : Form
     {
+        private Form2 myForm;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +21,15 @@ namespace SchoolRecords
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void BUTTON_Click(object sender, EventArgs e)
+        {
+            Person p1 = new Person("Rose Addison", 17);
+           // Student s1 = new Student("Bill Smith", 17, theDOS.Teacher);
+            Address a1 = new Address("HIghbrook", "Kingsthorn", "Herefordshire", "HR2 8AW");
+            myForm = new SchoolRecords.Form2(Person, Student);
+            myForm.Show();
         }
     }
 }
