76795e3875689c50c40f3d4a325d9262b76635ad
rsa8916 <46599719+rsa8916@users.noreply.github.com>
2019-03-20 10:00:10 +0000

    MyMusic
    title page

---
+++ code/MyMusic/MyMusic/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MyMusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginButton = new System.Windows.Forms.Button();
+            this.studentsUserName = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(215)))), ((int)(((byte)(96)))));
+            this.loginButton.Location = new System.Drawing.Point(291, 321);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(177, 70);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "LOGIN";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // studentsUserName
+            // 
+            this.studentsUserName.Location = new System.Drawing.Point(276, 260);
+            this.studentsUserName.Name = "studentsUserName";
+            this.studentsUserName.Size = new System.Drawing.Size(219, 20);
+            this.studentsUserName.TabIndex = 1;
+            this.studentsUserName.TextChanged += new System.EventHandler(this.studentsUserName_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MyMusic.Properties.Resources.spotify_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(241, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 216);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.studentsUserName);
+            this.Controls.Add(this.loginButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox studentsUserName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ code/MyMusic/MyMusic/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyMusic
+{
+    private mainCollection myCollection;
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        private void studentsUserName_TextChanged(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            myCollection = new MyMusic.mainCollection(studentsUserName);
+            Form.Show();
+            //myGame = new XOX.GameUI( playerOneName, playerTwoName);
+           // myGame.Show();
+        }
+    }
+}
