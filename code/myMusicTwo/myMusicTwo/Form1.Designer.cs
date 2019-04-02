79791c5c3557ac399d04946c2ff9761e6669144e
rsa8916 <46599719+rsa8916@users.noreply.github.com>
2019-04-02 13:40:24 +0100

    mm
    dunb

--- code/myMusicTwo/myMusicTwo/Form1.Designer.cs
+++ code/myMusicTwo/myMusicTwo/Form1.Designer.cs
@@ -28,56 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(173, 104);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(139, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "highway to hell - AC/DC";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(173, 139);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(189, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "ready to let go - Cage the elephant";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(173, 182);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(190, 17);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "Mr Blue sky - electriclight orchestra";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox4
-            // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(173, 234);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(126, 17);
-            this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "bad habit - the kooks";
-            this.checkBox4.UseVisualStyleBackColor = true;
             // 
             // listBox1
             // 
@@ -89,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(434, 300);
+            this.button1.Location = new System.Drawing.Point(446, 355);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -99,7 +55,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(178, 300);
+            this.button2.Location = new System.Drawing.Point(178, 373);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 6;
@@ -135,10 +91,6 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -147,11 +99,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
--- code/myMusicTwo/myMusicTwo/Form1.cs
+++ code/myMusicTwo/myMusicTwo/Form1.cs
@@ -15,110 +15,74 @@ namespace myMusicTwo
     public partial class Form1 : Form
     {
         string fileContent = Resources.music;
+        int nextFreeLocation = 0;
+        string[] tracks = new string[14];
+        CheckBox[] boxes;
+        String line;
+
+
         public Form1()
         {
             InitializeComponent();
+
             StringReader reader = new StringReader(fileContent);
+            while ((line = reader.ReadLine()) != null)
+            {
+                tracks[nextFreeLocation] = line;
+                nextFreeLocation++;
+            }
+
+            boxes = new CheckBox[15];
+            for (int i = 0; 1 < 14; i++)
+            {
+               boxes[i] = new CheckBox();
+               boxes[i].Text = line;
+
+            }
+           
+            
         }
 
-      //  String line;
         
-           //   while ((line = reader.ReadLine()) != null) 
+        private void allocatingButtons()
+        {
+           
+            
+        }
 
-          //    { 
-
-
-        
-         //              tracks[nextFreeLocation] = line; 
-
-            //           nextFreeLocation++; 
-
-          // }
-          
-
-    private void button1_Click(object sender, EventArgs e) //load button
+        private void button1_Click(object sender, EventArgs e) //load button
         {
             listBox1.Items.Clear();// cleared of "listBox1"
+            //my old text boxs
+          //  if (checkBox1.Checked)//condtion
+          //      listBox1.Items.Add(checkBox1.Text);//adding acdc 
+           // if (checkBox2.Checked)
+          //      listBox1.Items.Add(checkBox2.Text);//ect
+          //  if (checkBox3.Checked)
+          //      listBox1.Items.Add(checkBox3.Text);
+          //  if (checkBox4.Checked)
+         //       listBox1.Items.Add(checkBox4.Text);
 
-            if (checkBox1.Checked)//condtion
-                listBox1.Items.Add(checkBox1.Text);//adding acdc 
-            if (checkBox2.Checked)
-                listBox1.Items.Add(checkBox2.Text);//ect
-            if (checkBox3.Checked)
-                listBox1.Items.Add(checkBox3.Text);
-            if (checkBox4.Checked)
-                listBox1.Items.Add(checkBox4.Text);
+
+           
         }
 
         private void button2_Click(object sender, EventArgs e)//clear button
         {
             listBox1.Items.Clear();//clearing the list box :)
-            checkBox1.Checked = false;
-            checkBox2.Checked = false;
-            checkBox3.Checked = false;
-            checkBox4.Checked = false;
+                                   // checkBox1.Checked = false;
+                                   // checkBox2.Checked = false;
+                                   // checkBox3.Checked = false;
+                                   //checkBox4.Checked = false;
+            for (int n = 0; n < 14; n++)
+            {
+                boxes[n].Checked = false;//clears the check boxes 
+            }
+            
         }
 
-       
-
-         
-
-        // read in the strings and store in my tracksarray 
-
-         
-        
-        // Whilst we would normally associate a file with a path e.g.C:/users/mbr this technique is rarely portable as all of our paths are different. 
-
-
-        //It is better instead to put the file in Resources and access it from there
 
 
 
-
-        //first create your file using notepad, enter your titles, one per line; now save as music.txt
-
-
-
-
-
-        //In Visual Studio, once you've created your project go to:  
-
-        // Project -> <name of project> properties -> Resources
-
-        // choose Files from the pull down
-
-        //Add existing file(then browse to a file e.g.music) that you have already created 
-
-
-
-        // add these to Using at the top 
-
-        //  using System.IO;                // You need this for file handling //done
-
-        //   using MyMusic.Properties;   // You need this to access your Resources //done
-
-
-
-
-
-        //  string fileContent = Resources.music;
-
-        //  StringReader reader = new StringReader(fileContent);
-
-        // read in the strings and store in my tracksarray 
-
-        //  String line;  
-
-        //      while ((line = reader.ReadLine()) != null) 
-
-        //      { 
-
-
-
-        //                  tracks[nextFreeLocation] = line; 
-
-        //                nextFreeLocation++; 
-
-        //} 
     }
 }
