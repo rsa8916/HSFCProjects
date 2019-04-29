bfbf21ee53d3d3c59d86a58a085ad07b42a3f617
rsa8916 <46599719+rsa8916@users.noreply.github.com>
2019-04-29 11:06:49 +0100

    gameBreakout
    begining of game breakout

---
+++ code/Breakout GAme picturebox/pictureBoxGameBreakout/pictureBoxGameBreakout/GameObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+
+namespace pictureBoxGameBreakout
+{
+    public class GameObject
+    {
+        protected bool blnIsVisible;
+        protected Rectangle recPosition;
+        protected System.Drawing.Brush drawingBrushColour;
+
+        public GameObject(int x, int y, 
+                          int width, int height, 
+                          bool blnIsVisable, Brush drawingBrushColour)
+        {
+            this.blnIsVisible = blnIsVisible;//this. refers to to the property of blnIsVisible
+            recPosition = new Rectangle(x, y, width, height);
+            this.drawingBrushColour = drawingBrushColour;
+        }
+
+        public bool GetBlnIsVisible()
+        {
+            return blnIsVisible;
+        }
+        public Rectangle GetRecPosition()
+        {
+            return recPosition;
+        }
+        public Brush GetDrawingsBrushColour()
+        {
+            return drawingBrushColour;
+        }
+        public void SetBlnIsVisible(bool isVisible)
+        {
+            blnIsVisible = isVisible;
+        }
+        public void SetRecPosition(Rectangle position)
+        {
+            recPosition = position;
+        }
+        public void SetDrawings
+    }
+}
